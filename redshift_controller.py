238a4026ca5bd2a955483213fe550dce2fd72a9e
JehyunP <parkjehyun0131@gmail.com>
2025-10-29 13:57:40 +0900

    added function in reshift controller

--- redshift_controller.py
+++ redshift_controller.py
@@ -20,7 +20,10 @@ class redshift_controller:
         self.command_map = {
             'create_table_stn_sfc' : self.create_table_stn_sfc,
             'create_table_merged_sfc' : self.create_table_merged_sfc,
-            'create_table_merged_marine' : self.create_table_merged_marine
+            'create_table_merged_marine' : self.create_table_merged_marine,
+            'create_table_elt_sfc' : self.create_table_elt_sfc,
+            'create_table_elt_marine' : self.create_table_elt_marine,
+            'create_table_sfc_temp_change' : self.create_table_sfc_temp_change
         }
 
         # If Today's log not exist, create a new txt else just use established file
@@ -129,16 +132,30 @@ class redshift_controller:
             Create the table raw_data.stn_info if not exists,
             copy from S3 called stn_SFC_info.csv
         '''
+        query = '''
+            DROP TABLE IF EXISTS raw_data.stn_sfc CASCADE;
+            CREATE TABLE raw_data.stn_sfc (
+                stn_id        INT            PRIMARY KEY,    
+                lon_degree    FLOAT,                        
+                lat_degree    FLOAT,                       
+                stn_sp        VARCHAR(20),                
+                ht_m          FLOAT,                   
+                ht_pa_m       FLOAT,                  
+                ht_ta_m       FLOAT,                    
+                ht_wd_m       FLOAT,                    
+                ht_rn_m       FLOAT,                     
+                stn_ad        VARCHAR(50),            
+                stn_ko        VARCHAR(50),                 
+                stn_en        VARCHAR(50),                  
+                fct_id        VARCHAR(20),                  
+                law_id        VARCHAR(20)                   
+            )
+            DISTSTYLE KEY
+            DISTKEY (stn_id)
+            SORTKEY (stn_id);
+        '''
         try:
-            self.cursor.execute('''
-                drop table if exists raw_data.stn_sfc;
-                create table raw_data.stn_sfc (
-                    stn_id INT primary key,
-                    lon_degree FLOAT,
-                    lat_degree FLOAT,
-                    stn_ko VARCHAR(50)
-                );
-            ''')
+            self.cursor.execute(query)
 
             self.conn.commit()
             print('The stn sfc data created!')
@@ -176,23 +193,66 @@ class redshift_controller:
         '''
         try:
             create_sql = '''
-                drop table if exists raw_data.merged_sfc;
-                create table raw_data.merged_sfc(
-                    tm Date, 
-                    stn Int,
-                    pa float,
-                    ps FLOAT,
-                    pr FLOAT,
-                    ta FLOAT,
-                    td FLOAT,
-                    hm FLOAT,
-                    pv FLOAT,
-                    rn FLOAT,
-                    rn_int FLOAT,
-                    sd_hr3 FLOAT,
-                    sd_day FLOAT,
-                    sd_tot FLOAT,
-                    ts FLOAT,
+                DROP TABLE IF EXISTS raw_data.merged_sfc;
+                CREATE TABLE raw_data.merged_sfc (
+                    tm DATE,
+                    stn INT,
+
+                    ws_avg FLOAT,
+                    wr_day FLOAT,
+                    wd_max FLOAT,
+                    ws_max FLOAT,
+                    ws_max_tm VARCHAR(20),
+                    wd_ins FLOAT,
+                    ws_ins FLOAT,
+                    ws_ins_tm VARCHAR(20),
+                    ta_avg FLOAT,
+                    ta_max FLOAT,
+                    ta_max_tm VARCHAR(20),
+                    ta_min FLOAT,
+                    ta_min_tm VARCHAR(20),
+                    td_avg FLOAT,
+                    ts_avg FLOAT,
+                    tg_min FLOAT,
+                    hm_avg FLOAT,
+                    hm_min FLOAT,
+                    hm_min_tm VARCHAR(20),
+                    pv_avg FLOAT,
+                    ev_s FLOAT,
+                    ev_l FLOAT,
+                    fg_dur FLOAT,
+                    pa_avg FLOAT,
+                    ps_avg FLOAT,
+                    ps_max FLOAT,
+                    ps_max_tm VARCHAR(20),
+                    ps_min FLOAT,
+                    ps_min_tm VARCHAR(20),
+                    ca_tot FLOAT,
+                    ss_day FLOAT,
+                    ss_dur FLOAT,
+                    ss_cmb FLOAT,
+                    si_day FLOAT,
+                    si_60m_max FLOAT,
+                    si_60m_max_tm VARCHAR(20),
+                    rn_day FLOAT,
+                    rn_d99 FLOAT,
+                    rn_dur FLOAT,
+                    rn_60m_max FLOAT,
+                    rn_60m_max_tm VARCHAR(20),
+                    rn_10m_max FLOAT,
+                    rn_10m_max_tm VARCHAR(20),
+                    rn_pow_max FLOAT,
+                    rn_pow_max_tm VARCHAR(20),
+                    sd_new FLOAT,
+                    sd_new_tm VARCHAR(20),
+                    sd_max FLOAT,
+                    sd_max_tm VARCHAR(20),
+                    te_05 FLOAT,
+                    te_10 FLOAT,
+                    te_15 FLOAT,
+                    te_30 FLOAT,
+                    te_50 FLOAT,
+
                     PRIMARY KEY (tm, stn),
                     FOREIGN KEY (stn) REFERENCES raw_data.stn_sfc(stn_id)
                 )
@@ -286,6 +346,130 @@ class redshift_controller:
         except Exception as e:
             print(f'Error Creating Table : {e}')
             self.save_log('Error creating table: {e}')
+
+
+    def create_table_elt_sfc(self):
+        '''
+            Extract data from raw_data to create visualization 
+            SFC data sets will be used to transfer into analytics
+        '''
+        try:
+            create_sql = '''
+                drop table if exists analytics.sfc;
+                CREATE TABLE analytics.sfc as
+                    select
+                        a.tm as "관측시각",
+                        a.stn as "Station",
+                        b.stn_ko as "지역",
+                        b.lon_degree as "경도",
+                        b.lat_degree as "위도",
+                        a.ta_avg as "평균기온",
+                        a.ta_max as "최고기온",
+                        a.ta_min as "최저기온",
+                        a.hm_avg as "평균습도",
+                        a.rn_d99 as "강수량"
+                    from raw_data.merged_sfc a
+                    right join raw_data.stn_sfc as b on a.stn = b.stn_id
+                    order by a.tm;
+                '''
+            self.cursor.execute(create_sql)
+
+            self.conn.commit()
+            print('ELT Done : Created sfc table!')
+            self.save_log('THE analytics.sfc table created')
+
+
+        except Exception as e:
+            print(f'Error Creating Table : {e}')
+            self.save_log('Error creating table: {e}')
+
+
+    def create_table_elt_marine(self):
+        '''
+            Extract data from raw_data to create visualization 
+            marine data sets will be used to transfer into analytics
+        '''
+        try:
+            create_sql = '''
+                drop table if exists analytics.marine;
+
+                CREATE TABLE analytics.marine as
+                    select
+                        tm_kst as "관측시각",
+                        stn_id as "ID",
+                        stn_ko as "지점",
+                        lon_deg as "경도",
+                        lat_deg as "위도",
+                        tw_c as "해수면 온도",
+                        ta_c as "기온",
+                        hm_percent as "습도"
+                    from raw_data.merged_marine
+                    order by 1;
+                '''
+            self.cursor.execute(create_sql)
+
+            self.conn.commit()
+            print('ELT Done : Created marine table!')
+            self.save_log('THE analytics.marine table created')
+
+
+        except Exception as e:
+            print(f'Error Creating Table : {e}')
+            self.save_log('Error creating table: {e}')
+
+
+    def create_table_sfc_temp_change(self):
+        '''
+            ELT -> elt sfc table into calculate each year's temperature differences
+        '''
+        try:
+            create_sql = '''
+                drop table if exists analytics.sfc_temp_diff;
+
+                create table analytics.sfc_temp_diff as
+                    with year_tmp as (
+                        select 
+                            date_part('year', "관측시각") as year,
+                            avg("최고기온") as high_temp,
+                            avg("평균기온") as avg_temp,
+                            avg("최저기온") as low_temp
+                        from analytics.sfc
+                        group by 1
+                        order by 1
+                    )
+
+                    select
+                        year, 
+                        round(high_temp, 2) as "최고기온",
+                        round(avg_temp, 2) as "평균기온",
+                        round(low_temp, 2) as "최저기온",
+                        COALESCE(
+                            ROUND(high_temp - LAG(high_temp, 1) OVER (ORDER BY year), 2),
+                            0
+                        ) AS "최고기온 변화량",
+                        
+                        COALESCE(
+                            ROUND(avg_temp - LAG(avg_temp, 1) OVER (ORDER BY year), 2),
+                            0
+                        ) AS "평균기온 변화량",
+                        
+                        COALESCE(
+                            ROUND(low_temp - LAG(low_temp, 1) OVER (ORDER BY year), 2),
+                            0
+                        ) AS "최저기온 변화량"
+                    from year_tmp
+                    order by 1;
+            '''
+            self.cursor.execute(create_sql)
+
+            self.conn.commit()
+            print('ELT Done : Created sfc_temp_diff table!')
+            self.save_log('THE analytics.sfc_temp_diff table created')
+
+        except Exception as e:
+            print(f'Error Creating Table : {e}')
+            self.save_log('Error creating table: {e}')
+
 
 
 
